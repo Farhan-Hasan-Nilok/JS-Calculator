0d1f8db52c57c853d7f7086b3704c7aa16ce4c28
Farhan-Hasan-Nilok <nilok8679@gmail.com>
2022-06-17 22:15:25 +0600

    finished js added

--- function.js
+++ function.js
@@ -1,55 +1,35 @@
-let display = document.getElementById("display").value;
 
+(function(){
+    
+    let display = document.querySelector(".calculator-display");
+    let buttons = document.querySelectorAll(".button");
+    let clear = document.querySelector(".btn-clear");
+    let equal = document.querySelector(".key-equal");
 
-document.getElementById("one").addEventListener("click", function () {
-    showValue(1);
-})
+    buttons.forEach(function(button){
+        button.addEventListener("click", function(e){
+            let value = e.target.dataset.num;
+            if(document.getElementById("display").innerText == '0'){
+                display.innerText = value;
+            }
+            else{
+            display.innerText += value;
+            }
+        })
 
-document.getElementById("two").addEventListener("click", function () {
-    showValue(2);
-})
+        equal.addEventListener("click", function(e){
+            if(display.innerText == "0"){
+                display.innerText = "0"
+            }
 
-document.getElementById("three").addEventListener("click", function () {
-    showValue(3);
-})
+            else{
+                let result = eval(display.innerText);
+                display.innerText = result;
+            }
+        })
 
-document.getElementById("four").addEventListener("click", function () {
-    showValue(4);
-})
-
-document.getElementById("five").addEventListener("click", function () {
-    showValue(5);
-})
-
-document.getElementById("six").addEventListener("click", function () {
-    showValue(6);
-})
-document.getElementById("seven").addEventListener("click", function(){
-    showValue(7);
-})
-
-document.getElementById("eight").addEventListener("click", function () {
-    showValue(8);
-})
-
-document.getElementById("nine").addEventListener("click", function () {
-    showValue(9);
-})
-
-
-document.getElementById("zero").addEventListener("click", function () {
-    showValue(0);
-})
-document.getElementById("clear").addEventListener("click", function(){
-    document.getElementById("display").innerText = "0";
-})
-
-function showValue(number){
-    if (document.getElementById("display").innerText == "0"){
-        document.getElementById("display").textContent = number
-    }
-
-    else{
-        document.getElementById("display").innerText +=number;
-    }
-}
+        clear.addEventListener("click", function(e){
+            display.innerText = "0";
+        })
+    })
+})();
